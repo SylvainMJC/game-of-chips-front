01d40803791891b9db580b3b4a2336e6c2d64ee6
Sylvain <sylvain.mj.conan@gmail.com>
2020-09-07 11:18:44 +0200

    starrt Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const { resolve } = require('path');
 
 module.exports = {
   entry: resolve('./GameOfChips.js'),
-  mode: 'development',
+  mode: 'production',
   watch: true,
   output: {
     filename: 'bundle.min.js',
