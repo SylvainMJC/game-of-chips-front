014b43506819f16573c00046c085e3166d83e985
Sylvain <sylvain.mj.conan@gmail.com>
2020-09-07 10:41:26 +0200

    start webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const { resolve } = require('path');
 
 module.exports = {
   entry: resolve('./loader.js'),
+  mode: 'development',
   output: {
     filename: 'loader.min.js',
     path: resolve('./'),
